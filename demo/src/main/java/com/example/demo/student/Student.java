9a763450987fed9271600f0fe3e9edfabc500a4a
Newrich Oyuga <newrichoyuga@gmail.com>
2024-01-18 08:33:36 +0300

    solved connection to database

--- demo/src/main/java/com/example/demo/student/Student.java
+++ demo/src/main/java/com/example/demo/student/Student.java
@@ -1,7 +1,5 @@
 package com.example.demo.student;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-
 import java.time.LocalDate;
 
 
@@ -39,42 +37,52 @@ public class Student {
     }
 
     public Long getId() {
+
         return id;
     }
 
     public void setId(Long id) {
+
         this.id = id;
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public String getEmail() {
+
         return email;
     }
 
     public void setEmail(String email) {
+
         this.email = email;
     }
 
     public LocalDate getDob() {
+
         return dob;
     }
 
     public void setDob(LocalDate dob) {
+
         this.dob = dob;
     }
 
     public Integer getAge() {
+
         return age;
     }
 
     public void setAge(Integer age) {
+
         this.age = age;
     }
 
